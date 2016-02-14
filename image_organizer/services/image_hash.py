50c3701f18662e2016e80bb3d10410588bbbfd5d
Will Hughes <willhughes01@gmail.com>
2016-02-14 01:24:31 -0800

    Adding service and renaming processor file

---
+++ image_organizer/services/image_hash.py
@@ -0,0 +1,10 @@
+from image_origanizer.processor import ImageHashProcessor
+
+
+class ImageHashService():
+    """Handles the ability to make image hashes"""
+
+    @classmethod
+    def hash_images_in_list(image_file_list):
+        """Hash all the images in a list"""
+        #TODO
